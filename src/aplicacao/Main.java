b107025aa44c3a67c704c9e1388d8e2370fd974a
clara <clara3.macedo@gmail.com>
2026-02-02 15:16:51 -0300

    classe entidade7 Vendedor

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,6 +1,9 @@
 package aplicacao;
 
 import entidades.Departamento;
+import entidades.Vendedor;
+
+import java.util.Date;
 
 public class Main {
     public static void main (String[] args){
@@ -8,5 +11,7 @@ public class Main {
         Departamento departamento = new Departamento(1, "livros");
         System.out.println(departamento);
 
+        Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, departamento);
+        System.out.println(vendedor);
     }
 }
---
+++ src/entidades/Vendedor.java
@@ -0,0 +1,91 @@
+package entidades;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class Vendedor implements Serializable {
+    private Integer id;
+    private String nome;
+    private String email;
+    private Date dataNascimento;
+    private Double salarioBase;
+
+    private Departamento departamento;
+
+    //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Vendedor(Integer id, String nome, String email, Date dataNascimento, Double salarioBase, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.salarioBase = salarioBase;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Vendedor vendedor = (Vendedor) o;
+        return Objects.equals(id, vendedor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "departamento=" + departamento +
+                ", salarioBase=" + salarioBase +
+                ", dataNascimento=" + dataNascimento +
+                ", email='" + email + '\'' +
+                ", nome='" + nome + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
